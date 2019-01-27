2c0166af6df35006cf81fae85c44eb75f3e10228
Jamie Winspear <Jamie.winspear@conversocial.com>
2019-01-27 14:28:15 +0000

    video importer in

--- boilerroom/boilerroomyoutube/api.py
+++ boilerroom/boilerroomyoutube/api.py
@@ -1,5 +1,21 @@
 import requests
+from boilerroomyoutube.models import YoutubeVideo
+
+BASE_URL = 'https://www.googleapis.com/youtube/v3/search'
 
 
-def get_latest_youtube_video():
-	
+def get_latest_youtube_video(channel_id, api_key):
+    extras = "maxResults=1&order=date&type=video"
+    return requests.get(BASE_URL + '?part=snippet&channelId={}&{})&key={}'
+                        .format(channel_id, extras, api_key)).json()
+
+
+def create_youtube_video(resp_json):
+    etag = resp_json['etag']
+    url = resp_json['items'][0]['snippet']['thumbnails']['default']['url']
+    title = resp_json['items'][0]['snippet']['title']
+    published_at = resp_json['items'][0]['snippet']['publishedAt']
+    channel_id = resp_json['items'][0]['snippet']['channelId']
+    return YoutubeVideo.objects.create(etag=etag, url=url, title=title,
+                                       published_at=published_at,
+                                       channel_id=channel_id)
--- boilerroom/boilerroomyoutube/models.py
+++ boilerroom/boilerroomyoutube/models.py
@@ -3,3 +3,7 @@ from django.db import models
 
 class YoutubeVideo(models.Model):
     url = models.TextField("url", null=False, blank=False)
+    etag = models.TextField("etag", null=False, blank=False)
+    title = models.TextField("title", null=False, blank=False)
+    published_at = models.DateTimeField("published_at", null=False, blank=False)
+    channel_id = models.TextField("channel_id", null=False, blank=False)
